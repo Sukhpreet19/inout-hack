a270f370a0e41a8503c8320e22a95c692ff8c19f
Sukhpreet19 <sukhpreetni@gmail.com>
2019-10-25 18:35:15 +0530

    my own work

--- simple-https-server.py
+++ simple-https-server.py
@@ -5,10 +5,10 @@
 #    python simple-https-server.py
 # then in your browser, visit:
 #    https://localhost:4443
-
+# here we are importing three files BaseHTTPServer,SimpleHTTPServer,ssl form other program or system which is stored somewhere
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
-
-httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+#here the three imported files are used to input and manipulate server data
+httpd = BaseHTTPServer.HTTPServer(('localhost', 4785), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
 httpd.serve_forever()
